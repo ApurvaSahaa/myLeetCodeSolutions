30c955e69377ab3c5021b1fc2fafac516643b1dd
Apurva Saha <124815996+ApurvaSahaa@users.noreply.github.com>
2023-05-11 16:02:47 -0400

    Time: 35 ms (90.75%), Space: 49.7 MB (80.18%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,35 +1,30 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>> ans = new LinkedList<List<Integer>>();
-        
-        for(int i = 0; i < nums.length - 2; i++){
-            if(i == 0 || (i > 0 && nums[i] != nums[i - 1])){
-                int target = 0 - nums[i];
-                int left = i + 1;
-                int right = nums.length - 1;
-                
-                while(left < right){
-                    if(nums[left] + nums[right] == target){
-                        List<Integer> tempAns = new ArrayList<>();
-                        tempAns.add(nums[i]);
-                        tempAns.add(nums[left]);
-                        tempAns.add(nums[right]);
-                        
-                        ans.add(tempAns);
-                        
-                        while(left < right && nums[left] == nums[left + 1]) left++;
-                        while(left < right && nums[right] == nums[right - 1]) right--;
-                        
-                        left++;
-                        right--;
-                    }
-                    else if(nums[left] + nums[right] > target) right--;
-                    else left++;
+        List<List<Integer>> res = new ArrayList<>();
+        for(int i = 0; i < nums.length && nums[i] <= 0; i++){
+            if(i==0 || nums[i-1] != nums[i]){
+                twoSumII(nums, i, res);
+            }
+        }
+        return res;
+    }
+    
+    void twoSumII(int[] nums, int i, List<List<Integer>> res){
+        int left = i+1;
+        int right = nums.length - 1;
+
+        while(left < right){
+            int sum = nums[i] + nums[left] + nums[right];
+
+            if(sum < 0) left++;
+            else if(sum > 0) right--;
+            else{
+                res.add(Arrays.asList(nums[i], nums[left++], nums[right--]));
+                while(left < right && nums[left] == nums[left - 1]){
+                    left++;
                 }
             }
         }
-        
-        return ans;
     }
 }
