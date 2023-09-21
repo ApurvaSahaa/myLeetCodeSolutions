f5a79ac8b1b4824833e591dc30c64652b277859c
Apurva Saha <124815996+ApurvaSahaa@users.noreply.github.com>
2023-09-21 16:22:51 -0400

    Time: 1138 ms (48.39%), Space: 0B (100.00%) - LeetHub

--- 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
+++ 0511-game-play-analysis-i/0511-game-play-analysis-i.sql
@@ -1,4 +1,4 @@
-# Write your MySQL query statement below
-select player_id, min(event_date) as first_login
+/* Write your PL/SQL query statement below */
+select player_id, to_char(min(event_date), 'yyyy-mm-dd') as first_login
 from Activity
 group by player_id
