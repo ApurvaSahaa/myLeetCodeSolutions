ded538615de58ed06f71556c7d7462e0027518ec
Apurva Saha <124815996+ApurvaSahaa@users.noreply.github.com>
2023-10-06 16:16:46 -0400

    Time: 3 ms (67.52%), Space: 43.5 MB (79.91%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,17 +1,27 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        
         Map<Integer, Integer> map = new HashMap<>();
-        int[] ans = new int[2];
-        for(int i = 0; i < nums.length; i++){
-            int diff = target - nums[i];
-
-            if(!map.containsKey(diff)) map.put(nums[i],i);
-            else {
-                ans[0] = map.get(diff);
-                ans[1] = i;
+        
+        // a + b = target
+        // so we get a, and need to see if b is present in the hashmap
+        // b = target - a
+        // we store a as the key and the index as value
+        
+        for (int i = 0; i < nums.length; i++)
+        {
+            // a is nums[i] and we need to find b
+            int b = target - nums[i];
+            if (map.containsKey(b))
+            {
+                int[] ans = {i, map.get(b)};
+                return ans;
+            }
+            else
+            {
+                map.put(nums[i], i);
             }
         }
-
-        return ans;
+        return new int[2];
     }
 }
