241103bd777ad6df61f94aa9a0449b95836bd144
Apurva Saha <124815996+ApurvaSahaa@users.noreply.github.com>
2023-10-06 18:08:31 -0400

    Time: 5 ms (47.05%), Space: 55.3 MB (83.09%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,13 +1,39 @@
 class Solution {
     public int maxArea(int[] height) {
+        
+        // pseudocode
+        // we are given an unsorted integer array with numbers representing varying water levels
+        // we can use two pointers
+        // volume of water = (right pointer - left pointer) * minimum of number at (left pointer , right pointer)
+        // initialize pointers
+            // left pointer at the left extreme of array
+            // right pointer at the right extreme of array
+        // initialize a integer variable to contain the maximum volume till current loop
+        // check if left pointer < right pointer before entering a loop
+        // check the temporary volume and compare with max volume 
+            // if max volume < temp volume, assign temp volume to max volume
+            // if max volume >= temp volume, move ahead
+        // check and compare heights at left and right pointer
+            // if height at left pointer <= height at right pointer, left++
+            // else right--
+        // return max volume
+        
+        
+        //source code
+        
         int left = 0;
         int right = height.length - 1;
-        int maxAr = Integer.MIN_VALUE;
-        while(left < right){
-            maxAr = Integer.max(maxAr, ((right - left) * Integer.min(height[left], height[right])));
-            if(height[left] <= height[right]) left++;
+        int maxVolume = Integer.MIN_VALUE;
+        
+        while (left < right)
+        {
+            int tempVolume  = (right - left) * Integer.min(height[left], height[right]);
+            maxVolume  = Integer.max(maxVolume, tempVolume);
+            if (height[left] <= height[right]) left++;
             else right--;
         }
-        return maxAr;
+        
+        return maxVolume;
+        
     }
 }
