ce5a888e9bed9c792bec9a7ce00b2414b2064e15
Apurva Saha <124815996+ApurvaSahaa@users.noreply.github.com>
2023-10-10 19:08:25 -0400

    Time: 2 ms (82.15%), Space: 40.6 MB (39.57%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,14 +1,42 @@
 class Solution {
     public boolean isValid(String s) {
+        
+        // pseudocode
+        // we have a string that we will treal like a list of characters
+        // we will use the stack data structure
+        
+        // we loop over the string characters
+            // if any of the characters are one of the opening type we push its corresponding closing type into a stack
+            // else we check the character with the stack.pop and if they are equal we move on or we return false
+            // we just return the boolean value of stack.isEmpty()
+        
+        // source code
+        
         Stack<Character> stack = new Stack<>();
         
-        if(s.length() == 0 || s.length() == 1) return false;
+        if (s.length() == 0 || s.length() == 1)
+        {
+            return false;
+        }
         
-        for(char c : s.toCharArray()){
-            if (c == '(') stack.push(')');
-            else if (c == '{') stack.push('}');
-            else if (c == '[') stack.push(']');
-            else if (stack.isEmpty() || stack.pop() != c) return false;
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (s.charAt(i) == '(')
+            {
+                stack.push(')');
+            }
+            else if (s.charAt(i) == '{')
+            {
+                stack.push('}');
+            }
+            else if (s.charAt(i) == '[')
+            {
+                stack.push(']');
+            }
+            else if (stack.isEmpty() || s.charAt(i) != stack.pop())
+            {
+                return false;
+            }
         }
         
         return stack.isEmpty();
