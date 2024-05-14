1784fae87254d6397ca6fdf531b047dad739465a
Apurva Saha <124815996+ApurvaSahaa@users.noreply.github.com>
2024-05-14 16:06:40 -0400

    Time: 2 ms (97.61%), Space: 44.7 MB (54.69%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -3,25 +3,13 @@ class Solution {
         
         Map<Integer, Integer> map = new HashMap<>();
         
-        // a + b = target
-        // so we get a, and need to see if b is present in the hashmap
-        // b = target - a
-        // we store a as the key and the index as value
-        
-        for (int i = 0; i < nums.length; i++)
-        {
-            // a is nums[i] and we need to find b
+        for(int i = 0; i < nums.length; i++){
             int b = target - nums[i];
-            if (map.containsKey(b))
-            {
-                // only creating the array inside this block since we always have an answer
+            if(map.containsKey(b)){
                 int[] ans = {i, map.get(b)};
                 return ans;
             }
-            else
-            {
-                map.put(nums[i], i);
-            }
+            else map.put(nums[i], i);
         }
         return new int[2];
     }
