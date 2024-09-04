80196049562b7e9e231bc805df7a3456ffeafb26
Apurva Saha <124815996+ApurvaSahaa@users.noreply.github.com>
2024-09-04 19:25:51 -0400

    Time: 32 ms (73.89%), Space: 16.4 MB (61.89%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,13 +1,15 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
         '''
+        # recursion --> TLE here
         if n == 0  or n == 1:
             return 1
         
         return self.climbStairs(n-1) + self.climbStairs(n-2)
         
         '''
-        
+        '''
+        # memoization
         memo = {}
         return self.helper(n, memo)
     
@@ -19,4 +21,18 @@ class Solution:
             memo[n] = self.helper(n-1, memo) + self.helper(n-2, memo)
         return memo[n]
         
+        '''
+        
+        # dynamic programming
+        
+        if n == 0 or n == 1:
+            return 1
+        dp = [0] * (n+1)
+        dp[0] = dp[1] = 1
+        for i in range(2, n + 1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[-1]
+        
+        
+            
         
