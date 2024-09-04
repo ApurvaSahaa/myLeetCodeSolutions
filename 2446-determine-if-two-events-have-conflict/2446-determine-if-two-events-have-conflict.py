843beaa34268ce6228c9cb64bdaa5da367ff1ddd
Apurva Saha <124815996+ApurvaSahaa@users.noreply.github.com>
2024-09-04 14:48:02 -0400

    Time: 45 ms (5.42%), Space: 16.6 MB (25.00%) - LeetHub

--- 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.py
+++ 2446-determine-if-two-events-have-conflict/2446-determine-if-two-events-have-conflict.py
@@ -1,3 +1,10 @@
 class Solution:
     def haveConflict(self, event1: List[str], event2: List[str]) -> bool:
         return event1[0] <= event2[1] and event2[0] <= event1[1]
+    
+    # event 1 must start before event 2 ends
+    # event 2 must start before event 1 ends
+    
+    # for no conflict
+    # event 1 must start before event 2 ends
+    # event 2 must start after event 1 ends
