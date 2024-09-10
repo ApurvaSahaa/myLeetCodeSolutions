4fa92c3072f7e94e9ed20a21bd61d63eb22f2854
Apurva Saha <124815996+ApurvaSahaa@users.noreply.github.com>
2024-09-10 15:03:49 -0400

    Time: 154 ms (98.44%), Space: 20.8 MB (88.27%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.py
+++ 0344-reverse-string/0344-reverse-string.py
@@ -4,18 +4,20 @@ class Solution:
         Do not return anything, modify s in-place instead.
         """
         
-        '''
+        
         l, r = 0, len(s) - 1
         
         while l < r:
             s[l], s[r] = s[r], s[l]
             l += 1
             r -= 1
-        '''
         
+        '''
+        # not possible since we use separate memory for stack
         stack = []
         for i in s:
             stack.append(i)
         while stack:
             for i in range(0, len(s)):
                 s[i] = stack.pop()
+        '''
