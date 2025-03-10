9188372a9c2225fd7d7be34090ddb4c4704ea129
Apurva Saha <124815996+ApurvaSahaa@users.noreply.github.com>
2025-03-10 12:56:49 +0530

    Time: 11 ms (72.73%), Space: 17.9 MB (64.25%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -2,11 +2,13 @@ class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        
-        countS, countT = {}, {}
-        
-        for i in range(len(s)):
-            countS[s[i]] = 1 + countS.get(s[i], 0)
-            countT[t[i]] = 1 + countT.get(t[i], 0)
-        
-        return countS == countT
+        stmap = [0]*26
+        # s and t only contain lowercase english letters
+        for i in s:
+            stmap[ord(i) - ord('a')] += 1
+        for i in t:
+            stmap[ord(i) - ord('a')] -= 1
+        for i in stmap:
+            if i != 0:
+                return False
+        return True
