8b601489885c4a45b90fa1f48e16e1f78031ba5b
Apurva Saha <124815996+ApurvaSahaa@users.noreply.github.com>
2025-03-16 18:14:27 +0530

    Time: 103 ms (45.61%), Space: 26.9 MB (33.12%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,25 +1,12 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # going forward in time - for loop
-        # have a curr profit to maximize - start with 0
-        # for first day profit is zero
-        # for next day check if the curr price is greater than yesterdays price
-        # store yesterdays price as min price
-        # since curr price is greater profit becomes curr price minus min price
-        # update profit as max of curr profit and max profit
-        # if curr price is smaller than min price update min price
-        
-        
-        maxprofit = 0
-        minprice = prices[0]
-        
-        for i in prices:
-            if i > minprice:
-                currprofit = i - minprice
-                maxprofit = max(maxprofit, currprofit)
-            if i < minprice:
-                minprice = i
-                
-        return maxprofit
-                
-            
+        maxp = 0
+        l, r = 0, 1
+        while l < r and r < len(prices):
+            if prices[r] > prices[l]:
+                maxp = max(maxp, prices[r] - prices[l])
+                r += 1
+            else:
+                l = r
+                r += 1
+        return maxp
