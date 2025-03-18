6d6e2fbd770313b47c0bce918e475f48f813d57c
Apurva Saha <124815996+ApurvaSahaa@users.noreply.github.com>
2025-03-18 17:11:42 +0530

    Time: 0 ms (100%), Space: 19.1 MB (11.46%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -2,10 +2,9 @@ class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         
         nmap = {}
-
         for i in range(len(nums)):
-            diff = target - nums[i]
-            if diff in nmap.keys():
-                return [nmap[diff], i]
-            else:
-                nmap[nums[i]] = i
+            rem = target - nums[i]
+            if rem in nmap.keys():
+                return [i, nmap[rem]]
+            nmap[nums[i]] = i
+        return []
