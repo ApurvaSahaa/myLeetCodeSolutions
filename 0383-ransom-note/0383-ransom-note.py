e2f198cd5ada38572b505155e34638097bc773ac
Apurva Saha <124815996+ApurvaSahaa@users.noreply.github.com>
2025-03-19 21:37:09 +0530

    Time: 38 ms (7.59%), Space: 18 MB (54.58%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        wmap = {}
+        for i in ransomNote:
+            wmap[i] = wmap.get(i, 0) + 1
+        for i in magazine:
+            if i in wmap.keys():
+                wmap[i] -= 1
+        for v in wmap.values():
+            if v > 0:
+                return False
+        return True
