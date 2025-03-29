39ed10c2d2a9c3e669dd6c7bfbaecfde2e31a6f7
Apurva Saha <124815996+ApurvaSahaa@users.noreply.github.com>
2025-03-29 18:49:43 +0530

    Time: 7 ms (51.43%), Space: 21.3 MB (61.86%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -1,12 +1,11 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        fmap = {}
+        #edge case since there is always a unique solution here
+        if len(nums) == k:
+            return nums
+        #create the hashmap for elements and their frequencies
+        nmap = {}
         for i in nums:
-            if i in fmap.keys():
-                fmap[i] += 1
-            else:
-                fmap[i] = 1
-        
-        l = [[k, v] for k, v in fmap.items()]
-        l = sorted(l, key=lambda x:x[1], reverse=True)
-        return [l[i][0] for i in range(k)]
+            nmap[i] = nmap.get(i, 0) + 1
+        #building the max heap for k number of elements
+        return heapq.nlargest(k, nmap.keys(), key=nmap.get)
