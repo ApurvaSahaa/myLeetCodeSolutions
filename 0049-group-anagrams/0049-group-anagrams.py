1905efff91475fd073ee7f266e07d052c63fc68d
Apurva Saha <124815996+ApurvaSahaa@users.noreply.github.com>
2025-03-29 18:25:05 +0530

    Time: 11 ms (83.4%), Space: 20.7 MB (77.05%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,15 +1,13 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         out = []
-        if len(strs) == 0:
-            return [""]
-        wmap = {}
+        smap = {}
         for i in strs:
-            key = ''.join(sorted(i))
-            if key in wmap.keys():
-                wmap[key].append(i)
+            k = ''.join(sorted(i))
+            if k in smap.keys():
+                smap[k].append(i)
             else:
-                wmap[key] = [i]
-        for k, v in wmap.items():
+                smap[k] = [i]
+        for k, v in smap.items():
             out.append(v)
-        return out 
+        return out
