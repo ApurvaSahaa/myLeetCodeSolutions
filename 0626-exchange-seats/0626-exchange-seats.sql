28dbebe35536e121af48b716c6023baf0d444749
Apurva Saha <124815996+ApurvaSahaa@users.noreply.github.com>
2026-03-02 11:39:41 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,8 +1,17 @@
 -- Write your PostgreSQL query statement below
-select
+/*select
 case
     when s1.id % 2 <> 0 then coalesce((select s2.id from seat s2 where s1.id = s2.id - 1), s1.id)
     when s1.id % 2 = 0 then coalesce((select s2.id from seat s2 where s1.id = s2.id + 1), s1.id)
 end as id, student
 from seat s1
-order by id 
+order by id */
+
+select
+case
+    when id % 2 = 1 and id = (select max(id) from seat) then id
+    when id % 2 = 1 then id + 1
+    else id - 1
+end as id, student
+from seat
+order by id
