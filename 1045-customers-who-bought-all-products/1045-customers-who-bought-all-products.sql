396177fc728623fe921982e8ae96ae56f5619918
Apurva Saha <124815996+ApurvaSahaa@users.noreply.github.com>
2026-03-02 13:22:14 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
+++ 1045-customers-who-bought-all-products/1045-customers-who-bought-all-products.sql
@@ -1,6 +1,11 @@
 -- Write your PostgreSQL query statement below
-select customer_id
+/*select customer_id
 from (select customer_id, (count(distinct(product_key))) as products
 from customer
 group by customer_id)
-where products = (select count(*) from product)
+where products = (select count(*) from product)*/
+
+select customer_id
+from customer
+group by customer_id
+having count(distinct(product_key)) = (select count(*) from product)
