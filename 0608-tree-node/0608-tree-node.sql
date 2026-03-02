be2abb39d2089226a4d3e3f17318bc6becbb731c
Apurva Saha <124815996+ApurvaSahaa@users.noreply.github.com>
2026-03-02 09:19:28 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0608-tree-node/0608-tree-node.sql
+++ 0608-tree-node/0608-tree-node.sql
@@ -1,4 +1,7 @@
 -- Write your PostgreSQL query statement below
+-- in is generally more efficient than not in due to error from possible null values
+-- to overcome that we can use not in with a where column is not null condition
+-- or use in, not exists, or left join
 select id,
 case
     when p_id is null then 'Root'
