2743564971117d0624dd79d1fa6a0a0c3f497820
Apurva Saha <124815996+ApurvaSahaa@users.noreply.github.com>
2026-03-03 12:54:59 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,5 +1,5 @@
 -- Write your PostgreSQL query statement below
-select round(
+/*select round(
     100.0 * sum(case when order_date = customer_pref_delivery_date then 1 else 0 end) / count(*), 2
 ) as immediate_percentage
 from delivery
@@ -7,5 +7,11 @@ where (customer_id, order_date) in (
     select customer_id, min(order_date)
     from delivery
     group by customer_id
-)
+)*/
 
+select round(avg(case when order_date = customer_pref_delivery_date then 1 else 0 end)*100, 2) as immediate_percentage
+from delivery
+where (customer_id, order_date) in (
+    select customer_id, min(order_date)
+    from delivery group by customer_id
+)
