1142d03adc238f2a6b10ce3be0888e63d0d914c6
Apurva Saha <124815996+ApurvaSahaa@users.noreply.github.com>
2026-07-25 19:46:52 +0530

    Time: 251 ms (46.92%), Space: 0B (100%) - LeetHub

--- PostgreSQL/Medium/1934-confirmation-rate/1934-confirmation-rate.sql
+++ PostgreSQL/Medium/1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,23 +1,4 @@
 -- Write your PostgreSQL query statement below
-with messages as 
-(
-    select s.user_id, count(*)
-    from signups s left join confirmations c on s.user_id = c.user_id
-    group by s.user_id
-)
-,
-confirmed as 
-(
-    select c.user_id, case when c.action='confirmed' then 1 else 0 end as confirmed_message
-    from confirmations c
-)
-,
-final_cte as
-(
-    select m.user_id, m.count, sum(c.confirmed_message)
-    from messages m join confirmed c on m.user_id=c.user_id
-    group by m.user_id, m.count
-)
-
-select s.user_id, round(coalesce(fc.sum::numeric/fc.count, 0)::numeric, 2) as confirmation_rate
-from signups s left join final_cte fc on s.user_id = fc.user_id
+select s.user_id, round(avg(case when action = 'confirmed' then 1 else 0 end), 2) as confirmation_rate
+from signups s left join confirmations c on s.user_id = c.user_id
+group by s.user_id
